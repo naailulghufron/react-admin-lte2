dbc822ea254775d71ab2f4c16fd78b0b0cdeaa30
naailulghufron <naailul@yahoo.com>
2021-10-01 15:18:58 +0700

    install react-admin-lte 2

---
+++ src/components/DepartmentComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DepartmentComponent() {
+  return (
+    <div>
+      <h1>This is Department Page</h1>
+    </div>
+  )
+}
---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DepartmentComponent from './DepartmentComponent'
+
+export default function Layout() {
+  const _id = '12kk121j21kl21'
+  return (
+    <div>
+      <DepartmentComponent></DepartmentComponent>
+      <div>{_id}</div>
+    </div>
+  )
+}
